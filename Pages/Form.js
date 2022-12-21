f590495937eb151c421c96add7a0a4f91bf248da
Rodrigo Severo <rodrigoseverodev@gmail.com>
2022-12-21 01:09:06 -0400

    Primeiro commit

---
+++ Pages/Form.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import Picker from 'react-native-picker-select';
+import Header from './Header';
+import logo from './Assets/user_group_new.png';
+
+function Form() {
+    return (
+        <>
+            <Header title="Cadastro" />
+            <View style={styles.container}>
+                <Image source={logo} style={styles.topImage} />
+                <Text style={styles.title}>Preencha o formulário abaixo:</Text>
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.input} placeholder="Digite o nome" />
+                <TextInput style={styles.input} placeholder="Digite a idade" keyboardType={'numeric'} />
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Salvar</Text>
+                </TouchableOpacity>
+            </View>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+    },
+    inputContainer: {
+        margin: 20,
+        alignItems: 'stretch',
+    },
+    topImage: {
+        margin: 20,
+    },
+    title: {
+        fontSize: 20,
+    },
+    input: {
+        marginTop: 10,
+        height: 60,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        paddingHorizontal: 24,
+        fontSize: 16,
+        alignItems: 'stretch',
+    },
+    button: {
+        marginTop: 10,
+        height: 60,
+        backgroundColor: 'green',
+        borderRadius: 10,
+        paddingHorizontal: 24,
+        fontSize: 16,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+    }
+});
+
+export default Form;
---
+++ Pages/Home.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Header from './Header';
+
+export default function Home() {
+    const navigation = useNavigation();
+    const [contador, setContador] = useState(0);
+    function incrementar() {
+        setContador(contador + 1);
+    }
+
+    return (
+        <>
+            <Header title="Header Param" />
+            <View style={styles.container}>
+                <Text style={styles.title}>Hello World!</Text>
+                <Text>{contador} toques!</Text>
+                <TouchableOpacity style={styles.button} onPress={incrementar}>
+                    <Text>Press Here</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Form')}>
+                    <Text>Navegar</Text>
+                </TouchableOpacity>
+            </View>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: 'black'
+    },
+    button: {
+        marginTop: 20,
+        alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        padding: 10
+    }
+});
